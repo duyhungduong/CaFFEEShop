5eb83b4e2b1d79853a0918d7747e3af552fb57de
duyhungduong <duyhungduong10@gmail.com>
2024-09-14 22:17:58 +0700

    Day 6

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,11 +3,12 @@ const express = require("express");
 const router = express.Router();
 
 const userSignUpController = require("../controller/userSignUp");
-const userSignInController = require("../controller/userSignin");
+const userSignInController = require("../controller/userSignIn");
 const userDetailsController = require("../controller/userDetails");
 const authToken = require("../middleware/authToken");
 const userLogout = require("../controller/userLogout");
 const allUsers = require("../controller/allUsers");
+const updateUser = require("../controller/updateUser");
 
 router.post("/signup", userSignUpController);
 router.post("/signin", userSignInController);
@@ -16,5 +17,6 @@ router.get("/userLogout", userLogout);
 
 //admin
 router.get("/all-user", authToken, allUsers);
+router.post("/update-user", authToken, updateUser);
 
 module.exports = router;
